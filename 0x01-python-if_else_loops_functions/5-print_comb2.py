0812f65828d0f3e8950f60e67d49d3f2a3e84983
Muhammed Riad <mohamedreyad8811@gmail.com>
2024-03-22 18:53:21 +0200

    l

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,9 +2,8 @@
 # 5-print_comb2.py
 # Muhammed Riad
 
-for i in range(0, 10):
-    for y in range(0, 10):
-        if i != 9 and y != 9:
-            print("{}{}".format(i, y), end=", ")
-        elif i == 9 and y == 9:
-            print("{}{}".format(i, y), end="\n")
+for i in range(0, 100):
+    if i != 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:02d}".format(i), end="\n")
