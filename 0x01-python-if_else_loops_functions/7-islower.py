6cd1856fb7239d57f131b4d06590adacd0c91fc3
Muhammed Riad <mohamedreyad8811@gmail.com>
2024-03-22 19:16:04 +0200

    l

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Muhammed Riad
+
+def islower(c):
+    if c >= 'a' and c <= 'z':
+        return True
+    else:
+        return False
---
+++ 0x01-python-if_else_loops_functions/firstfun.py
@@ -0,0 +1,8 @@
+def islower(c):
+    if c >= 'a' and c <= 'z':
+        return True
+    else:
+        return False
+
+if islower('B') == True:
+    print("WORKS")
