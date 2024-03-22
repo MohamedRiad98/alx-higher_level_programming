afce38a1a86820da7954338dfabbe67db08f2a52
Muhammed Riad <mohamedreyad8811@gmail.com>
2024-03-22 18:58:40 +0200

    l

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Muhammed Riad
+
+for i in range(0, 9):
+    for j in range(1, 10):
+        if i != 8 and j != 9:
+            print("{}{}".format(i, j), end=", ")
+        elif i == 8 and j == 9:
+            print("{}{}".format(i, j), end="\n")
